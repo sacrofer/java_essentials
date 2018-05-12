1a481b295946f955deb3f72b85495cc17f0993db
sacrofer <38994980+sacrofer@users.noreply.github.com>
2018-05-12 11:28:25 -0500

    Updates to use FireFox

--- src/firstSelenium/FirstSelenium.java
+++ src/firstSelenium/FirstSelenium.java
@@ -1,13 +1,15 @@
 package firstSelenium;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstSelenium {
 
     public static void main(String[] args) {
-    	System.setProperty("webdriver.chrome.driver", "C:\\testautomation\\libs\\drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    	//System.setProperty("webdriver.chrome.driver", "C:\\testautomation\\libs\\drivers\\chromedriver.exe");
+    	System.setProperty("webdriver.gecko.driver", "C:\\testautomation\\libs\\drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
         String expectedTitle = "Welcome: Mercury Tours";
         driver.get("http://newtours.demoaut.com");
         String actualTitle = driver.getTitle();
